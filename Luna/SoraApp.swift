ad60ff231b01ae8e96bd2370e74d2c769b840302
Soupy-dev <04nihad17@gmail.com>
2026-03-02 19:13:20 -0500

    Fix tvOS build: wrap orientation lock APIs in #if !os(tvOS)

--- Luna/SoraApp.swift
+++ Luna/SoraApp.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+#if !os(tvOS)
     static var orientationLock: UIInterfaceOrientationMask = .all
 
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return AppDelegate.orientationLock
     }
+#endif
 
     func application(_ application: UIApplication,
                      handleEventsForBackgroundURLSession identifier: String,
